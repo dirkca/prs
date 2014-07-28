d482acf2e615786d024e44ea1c93e125158b6775
dirkca <meyer.dirk@gmail.com>
2014-07-27 21:19:55 -0600

    add prs theme

---
+++ layout/topmenu.php
@@ -0,0 +1,109 @@
+<div id="header">
+    <div id="quicklinks">
+      <div class="h-cont">
+      <ul>
+        <li class="last"><a href="#">Contact</a></li>
+        
+        
+        <li class="children sw"><a href="#">School Websites</a>
+              <ul class="collapsed" style="display: none;">
+              <li class="first"><a href="http://arrowwoodschool.ca">Arrowwood School</a></li>
+              <li class=""><a href="http://barons.prs26.ca">Barons School</a></li>
+              <li class=""><a href="http://brantchristianschool.ca">Brant Christian School</a></li>
+              <li class=""><a href="http://calgarychristianschool.com">Calgary Christian School</a></li>
+              <li class=""><a href="http://carmangay.prs26.ca">Carmangay Outreach School</a></li>
+              <li class=""><a href="http://championschool.ca">Champion School</a></li>
+              <li class=""><a href="http://coalhurstelementaryschool.ca">Coalhurst Elementary School</a></li>
+              <li class=""><a href="http://coalhursthighschool.ca">Coalhurst High School</a></li>
+              <li class=""><a href="http://countycentral.ca">County Central High School</a></li>
+              <li class=""><a href="http://dorothydalgliesh.ca">Dorothy Dalgliesh School</a></li>
+              <li class=""><a href="http://gcaschool.ca">Glenmore Christian Academy</a></li>
+              <li class=""><a href="http://hcacalgary.com">Heritage Christian Academy</a></li>
+              <li class=""><a href="http://huntsvilleschool.ca">Huntsville School</a></li>
+              <li class=""><a href="http://www.pallisersd.ab.ca/schools/hutterite-colony-schools">Hutterite Colony Schools</a></li>
+              <li class=""><a href="http://jees.ca">Jennie Emery Elementary School</a></li>
+              <li class=""><a href="http://johndavidson.prs26.ca">John Davidson School</a></li>
+              <li class=""><a href="http://kateandrewshighschool.com">Kate Andrews High School</a></li>
+              <li class=""><a href="http://masters.ab.ca">Master's Academy and College</a></li>
+              <li class=""><a href="http://mennosimonschristianschool.ca">Menno Simons Christian School</a></li>
+              <li class=""><a href="http://miloschool.ca">Milo School</a></li>
+              <li class=""><a href="http://noblecentralschool.ca">Noble Central School</a></li>
+              <li class=""><a href="http://www.palliserbeyondborders.com">Palliser Beyond Borders</a></li>
+              <li class=""><a href="http://passoutreach.ca">PASS+ Outreach School</a></li>
+              <li class=""><a href="http://pbhs.ca">Picture Butte High School</a></li>
+              <li class=""><a href="http://ribms.ca">R.I. Baker Middle School</a></li>
+              <li class=""><a href="http://sunnysideschool.ca">Sunnyside School</a></li>
+              <li class=""><a href="http://tcskids.com">Trinity Christian School</a></li>
+              <li class=""><a href="http://vulcanoutreach.prs26.ca">Vulcan Outreach School</a></li>
+              <li class=" last"><a href="http://vpes.ca">Vulcan Prairieview Elementary School</a></li>
+              </ul>
+              <div id="school_websites" class="select_contain">
+                    <select class="quicklinks_select">
+                    <option value="">Choose a Page</option>
+                    <option value="http://arrowwoodschool.ca">Arrowwood School</option>
+                    <option value="http://barons.prs26.ca">Barons School</option>
+                    <option value="http://brantchristianschool.ca">Brant Christian School</option>
+                    <option value="http://calgarychristianschool.com">Calgary Christian School</option>
+                    <option value="http://carmangay.prs26.ca">Carmangay Outreach School</option>
+                    <option value="http://championschool.ca">Champion School</option>
+                    <option value="http://coalhurstelementaryschool.ca">Coalhurst Elementary School</option>
+                    <option value="http://coalhursthighschool.ca">Coalhurst High School</option>
+                    <option value="http://countycentral.ca">County Central High School</option>
+                    <option value="http://dorothydalgliesh.ca">Dorothy Dalgliesh School</option>
+                    <option value="http://gcaschool.ca">Glenmore Christian Academy</option>
+                    <option value="http://hcacalgary.com">Heritage Christian Academy</option>
+                    <option value="http://huntsvilleschool.ca">Huntsville School</option>
+                    <option value="http://www.pallisersd.ab.ca/schools/hutterite-colony-schools">Hutterite Colony Schools</option>
+                    <option value="http://jees.ca">Jennie Emery Elementary School</option>
+                    <option value="http://johndavidson.prs26.ca">John Davidson School</option>
+                    <option value="http://kateandrewshighschool.com">Kate Andrews High School</option>
+                    <option value="http://masters.ab.ca">Master's Academy and College</option>
+                    <option value="http://mennosimonschristianschool.ca">Menno Simons Christian School</option>
+                    <option value="http://miloschool.ca">Milo School</option>
+                    <option value="http://noblecentralschool.ca">Noble Central School</option>
+                    <option value="http://www.palliserbeyondborders.com">Palliser Beyond Borders</option>
+                    <option value="http://passoutreach.ca">PASS+ Outreach School</option>
+                    <option value="http://pbhs.ca">Picture Butte High School</option>
+                    <option value="http://ribms.ca">R.I. Baker Middle School</option>
+                    <option value="http://sunnysideschool.ca">Sunnyside School</option>
+                    <option value="http://tcskids.com">Trinity Christian School</option>
+                    <option value="http://vulcanoutreach.prs26.ca">Vulcan Outreach School</option>
+                    <option value="http://vpes.ca">Vulcan Prairieview Elementary School</option>
+                    </select>
+                    <div class="select_mask">School Websites</div>
+                </div><!-- .select_contain -->
+            </li>
+                                
+           <li class="children"><a class="quicklinks" href="#">Link2</a></li>
+                                
+           <li class="children"><a class="quicklinks" href="#">Link1</a></li>
+                            
+           <li class="first">Quicklinks:</li>
+      </ul>
+      </div><!--h-cont-->
+    </div><!--quicklinks-->
+    
+    <div class="h-cont2">
+        <div class="logo_and_nav">
+            <div id="logo"><a href="/">Palliser</a></div>
+                <div id="main_nav2"><ul>
+                         <li class="navbar-text"><?php echo $OUTPUT->login_info() ?></li> 
+                </ul></div> 
+            <div class="clear"></div>
+        </div><!--logo_and_nav-->
+    </div><!--h-cont-->
+    
+    <div id="banner_images">
+        <div class="cont2">
+            <img class="img1" src="<?php echo $OUTPUT->pix_url('b_1', 'theme'); ?>" style="transform: rotate(0deg); top: 64px;" />
+            <img class="img2" src="<?php echo $OUTPUT->pix_url('b_2', 'theme'); ?>" style="transform: rotate(11deg); top: 67px;" />
+            <img class="img3" src="<?php echo $OUTPUT->pix_url('b_3', 'theme'); ?>" style="transform: rotate(-7deg); top: 21px;"/>
+           
+        </div><!-- cont2 -->
+        <div id="banner_overlay"></div>
+    </div><!-- #banner -->
+    
+  </div><!--#header--> 
+  
+  
+  
---
+++ settings.php
@@ -0,0 +1,55 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+defined('MOODLE_INTERNAL') || die;
+
+if ($ADMIN->fulltree) {
+
+    // Invert Navbar to dark background.
+    $name = 'theme_prs/invert';
+    $title = get_string('invert', 'theme_prs');
+    $description = get_string('invertdesc', 'theme_prs');
+    $setting = new admin_setting_configcheckbox($name, $title, $description, 0);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $settings->add($setting);
+
+    // Logo file setting.
+    $name = 'theme_prs/logo';
+    $title = get_string('logo','theme_prs');
+    $description = get_string('logodesc', 'theme_prs');
+    $setting = new admin_setting_configstoredfile($name, $title, $description, 'logo');
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $settings->add($setting);
+
+    // Custom CSS file.
+    $name = 'theme_prs/customcss';
+    $title = get_string('customcss', 'theme_prs');
+    $description = get_string('customcssdesc', 'theme_prs');
+    $default = '';
+    $setting = new admin_setting_configtextarea($name, $title, $description, $default);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $settings->add($setting);
+
+    // Footnote setting.
+    $name = 'theme_prs/footnote';
+    $title = get_string('footnote', 'theme_prs');
+    $description = get_string('footnotedesc', 'theme_prs');
+    $default = '';
+    $setting = new admin_setting_confightmleditor($name, $title, $description, $default);
+    $setting->set_updatedcallback('theme_reset_all_caches');
+    $settings->add($setting);
+}
